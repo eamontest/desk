2e20d790812d9342de68495085149bd927e1faf9
Eamon Straughn <es@techstackgroup.com>
2015-03-18 11:12:19 +0000

    taking an abstract approach, to functionality

--- calc.php
+++ calc.php
@@ -7,7 +7,20 @@
 
 class InvalidNumberException extends Exception;
 class DivisionByZeroException extends Exception;
+
 /*
  * Define what objects the calculator will have
+ * We're going to need operators
  */
+abstract class operator {
+	protected $precedence;
+	protected $operand;
 
+	public function getOperand(){
+		return $this->operand;
+	}
+
+	public function getPrecedence(){
+		return $this->precedence;
+	}
+}
