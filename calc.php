3ab97d07bf469bfe1fe9a5715b69e1fb4a4fe4f7
Eamon Straughn <es@techstackgroup.com>
2015-03-18 13:15:49 +0000

    starting again, had to step out quickly, adding calulation

--- calc.php
+++ calc.php
@@ -16,6 +16,8 @@ abstract class operator {
 	protected $precedence;
 	protected $operand;
 
+	public function __construct(){}
+
 	public function getOperand(){
 		return $this->operand;
 	}
@@ -34,6 +36,7 @@ abstract class operator {
  */
 class multiply extends operator {
 	public function __construct(){
+		parent::__construct();
 		$this->precedence = 1;
 		$this->operand = chr(42);
 	}
@@ -41,6 +44,7 @@ class multiply extends operator {
 
 class divide extends operator {
 	public function __construct(){
+		parent::__construct();
 		$this->precedence = 1;
 		$this->operand = chr(47);
 	}
@@ -48,6 +52,7 @@ class divide extends operator {
 
 class add extends operator {
 	public function __construct(){
+		parent::__construct();
 		$this->precedence = 2;
 		$this->operand = chr(43);
 	}
@@ -55,6 +60,7 @@ class add extends operator {
 
 class subtract extends operator {
 	public function __construct(){
+		parent::__construct();
 		$this->precedence = 2;
 		$this->operand = chr(45);
 	}
@@ -77,7 +83,7 @@ class number {
 	}
 
 	public function __toString(){
-		return $this->number;
+		return (string)$this->number;
 	}
 }
 
@@ -117,43 +123,77 @@ class equation {
 		$this->equation[] = new divide();
 		return $this;
 	}
-}
 
-
-/*
- * Next we are going to need a caller that will bring this all together
- * pattern in mind
- * (new calculator)->evaluate(
- (new equation)
- ->number(12)
- ->add()
- ->number(13)
- ->subtract()
- ->number(23);
- );
-
- or
-
- (new calculator)->evaluate()
- ->number(12)
- ->add()
- ->number(13)
- ->subtract()
- ->number(23);
- );
-
-
- */
-
-/*
- * Now we need an object to calculate, could posibily extend to manage multiple equations
- * Think it's think for me to get phpunit involve here now i've decided on best method from the options
- */
-class calculator {
-
-	public function evalute(equation $equation){
-
+	public function getExpression(){
+		return $this->equation;
 	}
 }
 
+/*
+ * Now we need an object to calculate, could posibily extend to manage multiple equations
+ * Think it's time for me to get phpunit involve here now i've decided on best method from the options
+ */
+class calculator {
+
+	protected $skipkey = null;
+
+	public function evaluate(equation $equation){
+
+		// store the functional operations
+		$fx = [];
+
+		// get our arguments
+		$args = (array)$equation->getExpression();
+
+		//iterate through the array
+		foreach($args as $key => $part){
+
+			// skip the keys we won't be needing
+			if($this->skipkey === $key) continue;
+
+			// get numbers
+			if(is_a($part, 'number')){
+				$fx[$key] = $part->getNumber();
+			}
+
+			// this is for the operators to examine the array
+			if(is_a($part, 'operator')){
+
+				if($part->getPrecedence() == 1){
+					$total = 0;
+					// lets calculate
+					if($part->getOperand() == '*')
+						$total += $args[$key-1]->getNumber() * $args[$key+1]->getNumber();
+					if($part->getOperand() == '/')
+						$total += $args[$key-1]->getNumber() / $args[$key+1]->getNumber();
+
+					// set this to the previous key
+					$fx[$key-1] = $total;
+
+					// remove this key
+					$this->skipkey = $key+1;
+				} else {
+					$fx[$key] = $part->getOperand();
+				}
+			}
+		}
+
+		// lets evaluate each of the remaining parts
+		$total = 0;
+		foreach($fx as $part){
+			$total .= $part;
+		}
+
+		// only allow digits at this stage
+		if(!preg_match('|[\d\+\_]+|', $total)){
+			return false;
+		}
+
+		// hate this function, strapped for time
+		eval('$total = ' .$total . ';');
+
+		// return the total
+		return $total;
+	}
+}
 
--- tests/test.php
+++ tests/test.php
@@ -2,9 +2,20 @@
 
 include 'calc.php';
 
+
+
 class CalculatorTest extends PHPUnit_Framework_TestCase {
 
 	public function operator(){
 
+		$value = (new calculator)->evaluate(
+				(new equation)
+				->number(12)
+				->add()
+				->number(13)
+				->subtract()
+				->number(23)
+		);
+		print $value;
 	}
 }
