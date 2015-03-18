f77e43159ad6a1f3792dda9806f084249de3c6a3
Eamon Straughn <es@techstackgroup.com>
2015-03-18 11:09:49 +0000

    lets add some exceptions, common ones to begin with

---
+++ calc.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Objective is to create a calculator
+ *
+ */
+
+class InvalidNumberException extends Exception;
+class DivisionByZeroException extends Exception;
+/*
+ * Define what objects the calculator will have
+ */
+
